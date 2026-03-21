f0a2431c3e5f486e4cc96057ea666783652287bd
Daniel Mendonça <danielmend@gmail.com>
2026-03-21 15:42:50 -0300

    adaptation for production execution in a specific folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/reports/" : "/",
   server: {
     host: "::",
     port: 8080,
