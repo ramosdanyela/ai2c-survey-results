5ac9410acb08c4a64866735ba1b3e56f2926add3
Daniel Mendonça <danielmend@gmail.com>
2026-03-23 15:49:17 -0300

    add script to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/reports/" : "/",
-  server: {
-    host: "::",
-    port: 8080,
-    // Configuração para React Router - redireciona todas as rotas para index.html
-    historyApiFallback: true,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: ["production", "staging"].includes(mode) ? "/reports/" : "/",
+    server: {
+      host: true,
+      port: parseInt(env.PORT),
+      // Configuração para React Router - redireciona todas as rotas para index.html
+      historyApiFallback: true,
     },
-  },
-  optimizeDeps: {
-    // Pré-empacota o módulo docx para evitar 504 (Outdated Optimize Dep) no export Word
-    include: ["docx"],
-  },
-}));
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    optimizeDeps: {
+      // Pré-empacota o módulo docx para evitar 504 (Outdated Optimize Dep) no export Word
+      include: ["docx"],
+    },
+  };
+});
